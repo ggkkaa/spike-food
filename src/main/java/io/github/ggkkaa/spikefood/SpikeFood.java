2161b74b7bcc5c5017c7d86d58d2335ffcf47be1
Luka <luka.talevski.hn@gmail.com>
2025-07-08 20:01:34 +0800

    Finish MVP

--- src/main/java/io/github/ggkkaa/spikefood/SpikeFood.java
+++ src/main/java/io/github/ggkkaa/spikefood/SpikeFood.java
@@ -2,12 +2,21 @@ package io.github.ggkkaa.spikefood;
 
 import com.mojang.logging.LogUtils;
 import io.github.ggkkaa.spikefood.recipe.ModRecipes;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.Tag;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.slf4j.Logger;
 
 import java.util.UUID;
@@ -30,7 +39,60 @@ public class SpikeFood
 
     @SubscribeEvent
     public void onItemCrafted(PlayerEvent.ItemCraftedEvent event) {
-        UUID playerUUID = event.getEntity().getUUID();
-        LOGGER.debug("UUID: {}", playerUUID);
+        ItemStack stack = event.getCrafting();
+        if (!stack.hasTag()) return;
+
+        CompoundTag tag = stack.getTag();
+        if (!tag.contains("Effect", Tag.TAG_COMPOUND)) return;
+        CompoundTag effect = tag.getCompound("Effect");
+
+        CompoundTag allEffects = tag.contains("Effects", Tag.TAG_COMPOUND)
+                ? tag.getCompound("Effects")
+                : new CompoundTag();
+
+        UUID uuid = event.getEntity().getUUID();
+        String uuidKey = uuid.toString();
+
+        ListTag effectList = new ListTag();
+        effectList.add(effect.copy());
+        allEffects.put(uuidKey, effectList);
+        tag.put("Effects", allEffects);
+        tag.remove("Effect");
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID)
+    public class CommonEvents {
+
+        @SubscribeEvent
+        public static void onFoodEaten(LivingEntityUseItemEvent.Finish event) {
+            ItemStack stack = event.getItem();
+            if (!stack.hasTag()) return;
+
+            CompoundTag tag = stack.getTag();
+            if (!tag.contains("Effects", Tag.TAG_COMPOUND)) return;
+
+            CompoundTag allEffects = tag.getCompound("Effects");
+
+            for (String key : allEffects.getAllKeys()) {
+                ListTag list = allEffects.getList(key, Tag.TAG_COMPOUND);
+
+                for (Tag raw : list) {
+                    if (!(raw instanceof CompoundTag effectTag)) continue;
+
+                    String id = effectTag.getString("id");
+                    int amplifier = effectTag.getInt("amplifier");
+                    int duration = effectTag.getInt("duration");
+
+                    ResourceLocation effectId = ResourceLocation.tryParse(id);
+                    if (effectId == null) continue;
+
+                    MobEffect effect = ForgeRegistries.MOB_EFFECTS.getValue(effectId);
+                    if (effect == null) continue;
+
+                    MobEffectInstance instance = new MobEffectInstance(effect, duration, amplifier);
+                    event.getEntity().addEffect(instance);
+                }
+            }
+        }
     }
 }
--- src/main/java/io/github/ggkkaa/spikefood/recipe/SpikeFoodRecipe.java
+++ src/main/java/io/github/ggkkaa/spikefood/recipe/SpikeFoodRecipe.java
@@ -1,138 +1,142 @@
 package io.github.ggkkaa.spikefood.recipe;
 
-import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import io.github.ggkkaa.spikefood.SpikeFood;
-import net.minecraft.core.NonNullList;
 import net.minecraft.core.RegistryAccess;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.GsonHelper;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.inventory.CraftingContainer;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.PotionItem;
+import net.minecraft.world.item.alchemy.Potion;
+import net.minecraft.world.item.alchemy.PotionUtils;
 import net.minecraft.world.item.crafting.*;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class SpikeFoodRecipe implements Recipe<CraftingContainer> {
-    private final NonNullList<Ingredient> inputItems;
-    private final ItemStack output;
+public class SpikeFoodRecipe implements CraftingRecipe {
     private final ResourceLocation id;
 
-    public SpikeFoodRecipe(NonNullList<Ingredient> inputItems, ItemStack output, ResourceLocation id) {
-        this.inputItems = inputItems;
-        this.output = output;
+    public SpikeFoodRecipe(ResourceLocation id) {
         this.id = id;
     }
 
 
     @Override
-    public boolean matches(CraftingContainer pContainer, Level pLevel) {
+    public boolean matches(@NotNull CraftingContainer pContainer, Level pLevel) {
         if (pLevel.isClientSide()) {
             SpikeFood.LOGGER.debug("The Level is Client Side!!!!");
             return false;
         }
 
-        boolean hasIngredient1 = false;
-        boolean hasIngredient2 = false;
+        boolean hasPotion = false;
+        boolean hasFoodItem = false;
 
         for (int i = 0; i < pContainer.getContainerSize(); i++) {
             ItemStack stack = pContainer.getItem(i);
 
             if (stack.isEmpty()) continue;
 
-            if (inputItems.get(0).test(stack)) {
-                hasIngredient1 = true;
-            } else if (inputItems.get(1).test(stack)) {
-                hasIngredient2 = true;
+            if (stack.isEdible()) {
+                hasFoodItem = true;
+            } else if (stack.getItem() instanceof PotionItem) {
+                hasPotion = true;
             } else {
-                SpikeFood.LOGGER.debug("Invalid item detected!");
                 return false;
             }
         }
-
-        SpikeFood.LOGGER.debug("Returning {}", hasIngredient1 && hasIngredient2);
-        return hasIngredient1 && hasIngredient2;
+        return hasFoodItem && hasPotion;
     }
 
     @Override
-    public ItemStack assemble(CraftingContainer pContainer, RegistryAccess access) {
-        return output.copy();
+    public @NotNull ItemStack assemble(@NotNull CraftingContainer pContainer, @NotNull RegistryAccess access) {
+        ItemStack potion = ItemStack.EMPTY;
+        ItemStack foodItem = ItemStack.EMPTY;
+
+        for (int i = 0; i < pContainer.getContainerSize(); i++) {
+            ItemStack stack = pContainer.getItem(i);
+
+            if (stack.isEmpty()) continue;
+
+            if (stack.isEdible()) {
+                foodItem = stack;
+            } else if (stack.getItem() instanceof PotionItem) {
+                potion = stack;
+            }
+        }
+
+        if (foodItem.isEmpty() || potion.isEmpty()) return ItemStack.EMPTY;
+
+        ItemStack result = foodItem.copy();
+        CompoundTag tag = result.getOrCreateTag();
+
+        Potion potionType = PotionUtils.getPotion(potion);
+        if(!potionType.getEffects().isEmpty()) {
+            MobEffectInstance effect = potionType.getEffects().get(0);
+            CompoundTag effectTag = new CompoundTag();
+            effectTag.putString("id", ForgeRegistries.MOB_EFFECTS.getKey(effect.getEffect()).toString());
+            effectTag.putInt("amplifier", effect.getAmplifier());
+            effectTag.putInt("duration", effect.getDuration());
+            tag.put("Effect", effectTag);
+        }
+
+        return result;
     }
 
     @Override
     public boolean canCraftInDimensions(int x, int y) {
-        return true;
+        return x * y >= 2;
     }
 
     @Override
-    public ItemStack getResultItem(RegistryAccess access) {
-        return output.copy();
+    public @NotNull ItemStack getResultItem(@NotNull RegistryAccess access) {
+        return new ItemStack(Items.BREAD);
     }
 
     @Override
-    public ResourceLocation getId() {
+    public @NotNull ResourceLocation getId() {
         return id;
     }
 
     @Override
-    public RecipeSerializer<?> getSerializer() {
+    public @NotNull RecipeSerializer<?> getSerializer() {
         return Serializer.INSTANCE;
     }
 
     @Override
-    public RecipeType<?> getType() {
-        return Type.INSTANCE;
+    public @NotNull RecipeType<?> getType() {
+        return RecipeType.CRAFTING;
     }
 
-    public static class Type implements RecipeType<SpikeFoodRecipe> {
-        public static final Type INSTANCE = new Type();
-        public static final String ID = "food_spiking";
+    @Override
+    public @NotNull CraftingBookCategory category() {
+        return CraftingBookCategory.MISC;
     }
 
     public static class Serializer implements RecipeSerializer<SpikeFoodRecipe> {
 
         public static final Serializer INSTANCE = new Serializer();
-        public static final ResourceLocation ID =
-            ResourceLocation.fromNamespaceAndPath(SpikeFood.MOD_ID, "food_spiking");
 
         @Override
-        public SpikeFoodRecipe fromJson(ResourceLocation id, JsonObject json) {
-            SpikeFood.LOGGER.debug("Turning JSON into SpikeFoodRecipe...");
-            ItemStack output = ShapedRecipe.itemStackFromJson(json);
-
-            JsonArray ingredients = GsonHelper.getAsJsonArray(json, "ingredients");
-            NonNullList<Ingredient> inputs = NonNullList.withSize(2, Ingredient.EMPTY);
-
-            for (int i = 0; i < inputs.size(); i++) {
-                inputs.set(i, Ingredient.fromJson(ingredients.get(i)));
-            }
-
-            SpikeFood.LOGGER.debug("Finished making SpikeFoodRecipe");
-            return new SpikeFoodRecipe(inputs, output, id);
+        public @NotNull SpikeFoodRecipe fromJson(@NotNull ResourceLocation id, JsonObject json) {
+            return new SpikeFoodRecipe(id);
         }
 
         @Override
-        public @Nullable SpikeFoodRecipe fromNetwork(ResourceLocation pRecipeId, FriendlyByteBuf pBuffer) {
-            NonNullList<Ingredient> inputs = NonNullList.withSize(pBuffer.readInt(), Ingredient.EMPTY);
-
-            for (int i = 0; i < inputs.size(); i++) {
-                inputs.set(i, Ingredient.fromNetwork(pBuffer));
-            }
-
-            ItemStack output = pBuffer.readItem();
-            return new SpikeFoodRecipe(inputs, output, pRecipeId);
+        public @Nullable SpikeFoodRecipe fromNetwork(@NotNull ResourceLocation pRecipeId, FriendlyByteBuf pBuffer) {
+            return new SpikeFoodRecipe(pRecipeId);
         }
 
         @Override
         public void toNetwork(FriendlyByteBuf pBuffer, SpikeFoodRecipe pRecipe) {
-            pBuffer.writeInt(pRecipe.inputItems.size());
-
-            for ( Ingredient ingredient : pRecipe.getIngredients() ) {
-                ingredient.toNetwork(pBuffer);
-            }
-
-            pBuffer.writeItemStack(pRecipe.getResultItem(null), false);
+            // Nothing to sync.
         }
 
     }
